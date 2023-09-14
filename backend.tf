88dbe31bbc62f8833d5d3abaf89e9a6252faa8c3
Elgamil, Abdelrahman <elgama@amazon.com>
2023-09-14 22:40:23 +0200

    dob-demo

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    # TODO: replace <alias> with your alias, e.g. terraform-backend-dob-elgama
+    # Hands-on 2 TODO: replace <alias> with your alias, e.g. terraform-backend-dob-elgama
     bucket = "terraform-backend-dob-elgama"
     key    = "terraform-aws-project"
     region = "eu-central-1"
