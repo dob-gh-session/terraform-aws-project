f43e05bd9a7389686135f8aad526cf4840abeb58
Elgamil, Abdelrahman <elgama@amazon.com>
2024-09-20 12:35:25 +0200

    Reset repos

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    # Hands-on 2 TODO: replace <alias> with your alias, e.g. terraform-backend-dob-elgama
-    bucket = "terraform-backend-dob-elgama"
+    # Hands-on 2: replace <alias> with your alias, e.g. terraform-backend-dob-elgama
+    bucket = "terraform-backend-dob-<alias>"
     key    = "terraform-aws-project"
     region = "eu-central-1"
   }
--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-module "s3" {
-  source         = "github.com/dob-gh-session/terraform-aws-s3-module"
-  s3_bucket_name = "dob-bucket-elgama"
+resource "aws_s3_bucket" "example" {
+  bucket_prefix = "my-tf-test-bucket-prefix"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
+
+# Hands-on 4: use the module from the repo terraform-aws-s3-module
